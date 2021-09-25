3f117ccf68998457b566948cac4005ff6df115ba
rick <rick_murphy@sohu.com>
2021-09-25 16:45:57 +0800

    new feature: multiple area in BaseForm
    add "sectionStart" property to BaseForm Item to seperate form to
    multiple area by <Divider/>

--- src/comp/form/BaseForm.tsx
+++ src/comp/form/BaseForm.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import { Form, Row, Col, Button, Space } from 'antd';
+import { Form, Row, Col, Button, Space, DividerProps, Divider } from 'antd';
 import { Data, Entity } from '../model';
 import { FormInstance, FormItemProps, FormProps } from 'antd/lib/form';
 import { EditorType } from '../editor/editors';
@@ -10,7 +10,8 @@ export interface IBaseFormItemProps extends FormItemProps {
   label: string,
   span: 1 | 2 | 3 | 4,
   editor?: EditorType<any>,
-  subItems?: IBaseFormItemProps[]
+  subItems?: IBaseFormItemProps[],
+  sectionStart?: Omit<DividerProps, 'type'> & {title?: string};  // all horizontal,
 }
 
 export interface IBaseFormProps<E extends (Entity | Data)> extends Omit<FormProps<E>, 'initialValues,onFinish'>{
@@ -54,30 +55,68 @@ const getColSpan = (colCount:number, span: number): number => {
 
 export const getLayout = (items: IBaseFormItemProps[], colCount: number): ReactNode[] => {
   let rows: ReactNode[] = [];
-  let start: number = 0, end: number = 0, span: number = 0;
+  let
+    start: number = 0,
+    end: number = 0,
+    span: number = 0;
+
   for(let i = 0; i < items.length; i++){
     end = i;
     let currCount = items[i].span > colCount ? colCount : items[i].span;
-    span += currCount;
-    if(span > colCount){
-      // 如超出宽度，将已计数的item归入Row记录
-      let rowElements = items.slice(start, end);
-      rows.push(
-        <Row key={i} gutter={24}>
-          {rowElements.map(e => (
-            <Col key={e.key} span={getColSpan(colCount, e.span)}>
-              {getItem(e)}
-            </Col>
-          ))}
-        </Row>
-      );
+    // 按需添加 Divider
+    if(undefined !== items[i].sectionStart){
+      if(end >= start){
+        // 尚未排版的项目
+        let rowElements = items.slice(start, end);
+        rows.push(
+          <Row key={"end"} gutter={24}>
+            {rowElements.map(e => (
+              <Col key={e.key} span={getColSpan(colCount, e.span)}>
+                {getItem(e)}
+              </Col>
+            ))}
+          </Row>
+        );
+      }
+      let t: string | undefined = items[i].sectionStart?.title;
+      let sdClone = Object.assign({}, items[i].sectionStart);
+      delete sdClone.title;
+      if(t !== undefined){
+        rows.push(
+          <Divider {...sdClone}>{t}</Divider>
+        );
+      } else {
+        rows.push(
+          <Divider {...sdClone}/>
+        );
+      }
       // 重置计数
       span = currCount;
       start = i;
+    } else {
+      span += currCount;
+      if(span > colCount){
+        // 如超出宽度, 或是区域起始点，则将已计数的item归入Row记录
+        let rowElements = items.slice(start, end);
+        if(rowElements.length > 0){
+          rows.push(
+            <Row key={i} gutter={24}>
+              {rowElements.map(e => (
+                <Col key={e.key} span={getColSpan(colCount, e.span)}>
+                  {getItem(e)}
+                </Col>
+              ))}
+            </Row>
+          );
+        }
+        // 重置计数
+        span = currCount;
+        start = i;
+      }
     }
   }
   if(end >= start){
-    // 最后不足一行的
+    // 最后sheng不足一行的
     let rowElements = items.slice(start);
     rows.push(
       <Row key={"end"} gutter={24}>
